c553dd9c0f443117cf2c19057cebb086cc5b11ca
mhlo@netcompany.com <53852490+Long2511@users.noreply.github.com>
2020-09-15 22:38:57 +0700

    Added the Server code

--- scr/TCPServer.java
+++ scr/TCPServer.java
@@ -1,7 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 public class TCPServer {
-    public static void main() {
-        Socket socket;
+
+    public static final int SERVER_PORT = 6969;
+
+    public static void main(String[] args) {
+        ServerSocket server;
+        try {
+            System.out.println("Binding to the port: " + SERVER_PORT);
+            server = new ServerSocket(SERVER_PORT);
+            System.out.println("Connected to port: " + SERVER_PORT);
+            System.out.println("Waiting for client....");
+        while (true){
+            System.out.println("");
+            Socket socket = server.accept();
+            System.out.println("Client connected. Ready to chat.");
+
+            OutputStream os = socket.getOutputStream();
+            InputStream is = socket.getInputStream();
+            int ch = 0;
+            while (true){
+                ch = is.read();
+                if (ch==-1){
+                    break;
+                }
+                os.write(ch);
+            }
+            socket.close();
+        }
+
+        } catch (IOException e) {
+            System.err.println("Connection error: "+ e);
+        }
     }
 }
